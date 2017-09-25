545b8d9540278c117b494a585b43b45f6b83c8e6
BeijiYang <langyuyang@outlook.com>
2017-09-25 15:29:24 +0800

    Authorization from server

--- api/api/auth.js
+++ api/api/auth.js
@@ -1,21 +1,10 @@
 const COS = require('cos-nodejs-sdk-v5')
 const config = require('../config/config')
 
-// const params = {
-//   AppId: `${config.AppId}`,
-//   SecretId: `${config.SecretId}`,
-//   SecretKey: `${config.SecretKey}`,
-//   // Bucket: `${config.Bucket}`,
-//   // Region: `${config.Region}`,
-//   // Output : 'WRITE_STREAM',
-//   Method: 'get',                          /* 必须 */
-// }
-//
-// const cos = new COS(params)
-// const Authorization = cos.getAuth(params);
 exports.auth = (req, res) => {
-  console.log('auth');
-  console.log(req.body.method);
+  console.log('auth')
+  // console.log(req.body.method);
+  // console.log(req.body.pathname);
   const params = {
     AppId: `${config.AppId}`,
     SecretId: `${config.SecretId}`,
@@ -23,12 +12,13 @@ exports.auth = (req, res) => {
     // Bucket: `${config.Bucket}`,
     // Region: `${config.Region}`,
     // Output : 'WRITE_STREAM',
-    Method: req.body.method,                          /* 必须 */
+    Method: req.body.method,
+    Key: req.body.pathname
   }
 
   const cos = new COS(params)
 
   const Authorization = cos.getAuth(params)
-  console.log(Authorization);
+  // console.log(Authorization)
   res.status(200).json(Authorization)
 }
